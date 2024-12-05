a18029ff81e0192bb8955390d41be70c82419ed6
kanji1992 <kanjibhanderi@gmail.com>
2024-12-05 16:09:00 +0530

    added error hadling for negative inputs and added custum delimiter logic and theri test cases as well

--- services/string-calculator.js
+++ services/string-calculator.js
@@ -1,7 +1,18 @@
 class StringCalculator {
   add(number) {
     if (!number) return 0;
-    const arry = number.replace(/\n/g,",")  .split(",").map(Number);
+    let delimiter = /,|\n/;
+    if (number.startsWith("//")) {
+      const parts = number.split("\n");
+      delimiter = new RegExp(parts[0].slice(2));
+      number = parts[1];
+    }
+    const arry = number.split(delimiter).map(Number);
+    const negatives = arry.filter((num) => num < 0);
+    if (negatives.length) {
+      throw new Error(`negative numbers not allowed: ${negatives.join(",")}`);
+    }
+
     return arry.reduce((totalSum, number) => totalSum + number, 0);
   }
 }
--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -9,14 +9,22 @@ test("shuld return single number if provided single", () => {
   expect(calculator.add("1")).toBe(1);
 });
 
-test("should return sum of multiple comma seperated numbers", ()=>{
-    expect(calculator.add("1,2,3,4,5,6,7")).toBe(28)
-})
+test("should return sum of multiple comma seperated numbers", () => {
+  expect(calculator.add("1,2,3,4,5,6,7")).toBe(28);
+});
 
-test("should handle new line as seperator",()=>{
-    expect(calculator.add("3\n2\n1,2")).toBe(8)
-})
+test("should handle new line as seperator", () => {
+  expect(calculator.add("3\n2\n1,2")).toBe(8);
+});
 
+test("should handle custom seperator", () => {
+  const calculator = new StringCalculator();
+  expect(calculator.add("//;\n1;2")).toBe(3);
+});
 
-  
-  
+test("should throw an error when negative numbers are included", () => {
+  const calculator = new StringCalculator();
+  expect(() => calculator.add("1,-2,3,-4")).toThrow(
+    "negative numbers not allowed: -2,-4"
+  );
+});
