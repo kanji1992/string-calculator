29aa1529d4b38a9e3d6b24309fb010ce37cd1d6c
kanji1992 <kanjibhanderi@gmail.com>
2024-12-05 14:38:00 +0530

    Added test case for should return single if send single or return sum of numbers if send multiple comma seperated

--- services/string-calculator.js
+++ services/string-calculator.js
@@ -1,6 +1,8 @@
 class StringCalculator {
-    add(number) {
-        if(!number) return 0;
-    }
+  add(number) {
+    if (!number) return 0;
+    const arry = number.split(",").map(Number);
+    return arry.reduce((totalSum, number) => totalSum + number, 0);
+  }
 }
-module.exports = StringCalculator
+module.exports = StringCalculator;
--- tests/string-calculator.test.js
+++ tests/string-calculator.test.js
@@ -1,5 +1,17 @@
-const StringCalculator = require('../services/string-calculator')
+const StringCalculator = require("../services/string-calculator");
+const calculator = new StringCalculator();
+
 test("should return 0 for an empty string", () => {
-    const calculator = new StringCalculator();
-    expect(calculator.add("")).toBe(0);
-  });
+  expect(calculator.add("")).toBe(0);
+});
+
+test("shuld return single number if provided single", () => {
+  expect(calculator.add("1")).toBe(1);
+});
+
+test("should return sum of multiple comma seperated numbers", ()=>{
+    expect(calculator.add("1,2,3,4,5,6,7")).toBe(28)
+})
+
+  
+  
